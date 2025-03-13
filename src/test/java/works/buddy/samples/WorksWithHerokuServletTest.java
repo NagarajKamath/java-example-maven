d92d3d48284995051a0ad9e619512f8ec5896c1c
NagarajKamath <71336337+NagarajKamath@users.noreply.github.com>
2025-03-13 22:59:33 +0530

    Update WorksWithHerokuServletTest.java

--- src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
+++ src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
@@ -36,6 +36,6 @@ public class WorksWithHerokuServletTest {
         when(response.getWriter()).thenReturn(writer);
 
         servlet.doGet(request, response);
-        assertEquals("WEB APP Created by Nagaraj is WORKING \n\n\t -Nagaraj", new String( out.toByteArray(), "UTF-8"));
+        assertEquals("WEB APP Created by Nagaraj is WORKING \n\n\t\n\n\t -Nagaraj", new String( out.toByteArray(), "UTF-8"));
     }
 }
